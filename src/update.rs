2a89a51670a15fd0198613078cf31b13ba7a68e9
Juliette Cordor <grassojames5@gmail.com>
2022-11-03 00:27:31 +1100

    added function to get config path

---
+++ src/update.rs
@@ -0,0 +1,16 @@
+use std::{env, path::PathBuf};
+
+fn get_config_path() -> PathBuf {
+    let xdg_config = env::var("XFG_CONFIG_HOME").map(PathBuf::from);
+    let user_profile = env::var("USERPROFILE").map(|path| PathBuf::from(path).join(".config"));
+
+    match (xdg_config, user_profile) {
+        (Ok(path), _) => path,
+        (_, Ok(path)) => path,
+        _ => panic!("Could not find config directory"),
+    }
+}
+
+fn main() {
+    let config_path = get_config_path();
+}
