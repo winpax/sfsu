0e7be9e4eff3d7fc79da01f9eb0147a09ebaa1bd
Juliette Cordor <grassojames5@gmail.com>
2022-11-03 00:29:26 +1100

    fixes

--- src/config.rs
+++ src/config.rs
@@ -1,1 +1,12 @@
+use std::{env, path::PathBuf};
 
+pub fn get_config_path() -> PathBuf {
+    let xdg_config = env::var("XFG_CONFIG_HOME").map(PathBuf::from);
+    let user_profile = env::var("USERPROFILE").map(|path| PathBuf::from(path).join(".config"));
+
+    match (xdg_config, user_profile) {
+        (Ok(path), _) => path,
+        (_, Ok(path)) => path,
+        _ => panic!("Could not find config directory"),
+    }
+}
