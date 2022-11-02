2c7446356f235d7293d49a38f832b09584eb4310
Juliette Cordor <grassojames5@gmail.com>
2022-11-03 00:35:26 +1100

    moved into impl

--- src/config.rs
+++ src/config.rs
@@ -10,31 +10,33 @@ pub struct ScoopConfig {
     pub scoop_branch: Option<String>,
 }
 
-pub fn get_config_path() -> PathBuf {
-    let xdg_config = env::var("XFG_CONFIG_HOME").map(PathBuf::from);
-    let user_profile = env::var("USERPROFILE").map(|path| PathBuf::from(path).join(".config"));
+impl ScoopConfig {
+    pub fn read() -> std::io::Result<ScoopConfig> {
+        let config_path = ScoopConfig::get_path();
 
-    let path = match (xdg_config, user_profile) {
-        (Ok(path), _) => path,
-        (_, Ok(path)) => path,
-        _ => panic!("Could not find config directory"),
-    }
-    .join("scoop")
-    .join("config.json");
+        let config = std::fs::read_to_string(config_path)?;
 
-    if !path.exists() {
-        panic!("Could not find config file");
+        let config: ScoopConfig = serde_json::from_str(&config)?;
+
+        Ok(config)
     }
 
-    path
-}
+    pub fn get_path() -> PathBuf {
+        let xdg_config = env::var("XFG_CONFIG_HOME").map(PathBuf::from);
+        let user_profile = env::var("USERPROFILE").map(|path| PathBuf::from(path).join(".config"));
 
-pub fn read_scoop_config() -> std::io::Result<ScoopConfig> {
-    let config_path = get_config_path();
+        let path = match (xdg_config, user_profile) {
+            (Ok(path), _) => path,
+            (_, Ok(path)) => path,
+            _ => panic!("Could not find config directory"),
+        }
+        .join("scoop")
+        .join("config.json");
 
-    let config = std::fs::read_to_string(config_path)?;
+        if !path.exists() {
+            panic!("Could not find config file");
+        }
 
-    let config: ScoopConfig = serde_json::from_str(&config)?;
-
-    Ok(config)
+        path
+    }
 }
