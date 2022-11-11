206303d2d9f80379a1f840e454329fd95e83046b
Juliette Cordor <grassojames5@gmail.com>
2022-11-11 15:36:22 +1100

    moved modules into seperate files

---
+++ src/buckets.rs
@@ -0,0 +1,35 @@
+use std::path::{Path, PathBuf};
+
+use crate::get_scoop_path;
+
+pub struct Bucket {
+    path: PathBuf,
+    repo: git2::Repository,
+}
+
+impl Bucket {
+    pub fn open(name: impl AsRef<Path>) -> Result<Self, git2::Error> {
+        let path = get_scoop_path().join("buckets").join(name);
+
+        let repo = git2::Repository::open(&path)?;
+
+        Ok(Self { path, repo })
+    }
+}
+
+pub fn get_buckets() -> std::io::Result<Vec<Bucket>> {
+    let mut buckets = vec![];
+
+    let bucket_path = get_scoop_path().join("buckets");
+
+    for bucket in bucket_path.read_dir()? {
+        let bucket = bucket?;
+        let bucket_name = bucket.file_name();
+
+        let bucket = Bucket::open(bucket_name).unwrap();
+
+        buckets.push(bucket);
+    }
+
+    Ok(buckets)
+}
--- src/lib.rs
+++ src/lib.rs
@@ -8,90 +8,9 @@ pub fn get_scoop_path() -> PathBuf {
     home_dir.join("scoop")
 }
 
-pub mod buckets {
-    use std::path::{Path, PathBuf};
+pub mod buckets;
 
-    use crate::get_scoop_path;
-
-    pub struct Bucket {
-        path: PathBuf,
-        repo: git2::Repository,
-    }
-
-    impl Bucket {
-        pub fn open(name: impl AsRef<Path>) -> Result<Self, git2::Error> {
-            let path = get_scoop_path().join("buckets").join(name);
-
-            let repo = git2::Repository::open(&path)?;
-
-            Ok(Self { path, repo })
-        }
-    }
-
-    pub fn get_buckets() -> std::io::Result<Vec<Bucket>> {
-        let mut buckets = vec![];
-
-        let bucket_path = get_scoop_path().join("buckets");
-
-        for bucket in bucket_path.read_dir()? {
-            let bucket = bucket?;
-            let bucket_name = bucket.file_name();
-
-            let bucket = Bucket::open(bucket_name).unwrap();
-
-            buckets.push(bucket);
-        }
-
-        Ok(buckets)
-    }
-}
-
-pub mod packages {
-    use std::{fs::File, io::Read, path::Path};
-
-    use serde::{Deserialize, Serialize};
-
-    use crate::get_scoop_path;
-
-    #[derive(Debug, Deserialize, Serialize)]
-    pub struct Manifest {
-        /// The version of the package
-        pub version: String,
-    }
-
-    #[derive(Debug, Deserialize, Serialize)]
-    pub struct InstallManifest {
-        /// The bucket the package was installed from
-        pub bucket: String,
-    }
-
-    pub fn is_installed(manifest_name: impl AsRef<Path>, bucket: Option<impl AsRef<str>>) -> bool {
-        let scoop_path = get_scoop_path();
-        let installed_path = scoop_path
-            .join("apps")
-            .join(manifest_name)
-            .join("current/install.json");
-
-        if installed_path.exists() {
-            if let Some(bucket) = bucket {
-                let mut buf = String::new();
-
-                File::open(installed_path)
-                    .unwrap()
-                    .read_to_string(&mut buf)
-                    .unwrap();
-
-                let manifest: InstallManifest = serde_json::from_str(&buf).unwrap();
-
-                manifest.bucket == bucket.as_ref()
-            } else {
-                true
-            }
-        } else {
-            false
-        }
-    }
-}
+pub mod packages;
 
 // Borrowed from https://github.com/rust-lang/git2-rs/blob/master/examples/pull.rs
 // The following is licensed under the MIT or the Apache-2.0 license, at your option.
