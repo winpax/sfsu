a1a0f04ffd2d52e963b418805fa041c630076c45
Juliette Cordor <grassojames5@gmail.com>
2022-11-11 15:54:59 +1100

    updates to list bin

--- src/bin/search.rs
+++ src/bin/search.rs
@@ -18,11 +18,11 @@ struct SearchArgs {
     #[clap(help = "The pattern to search for (can be a regex)")]
     pattern: Option<Regex>,
 
-    #[clap(long, help = "Print the Powershell hook")]
-    hook: bool,
-
     #[clap(short, long, help = "The bucket to exclusively search in")]
     bucket: Option<String>,
+
+    #[clap(long, help = "Print the Powershell hook")]
+    hook: bool,
 }
 
 // TODO: Add installed marker
