e988db4ca7cc6c0bb857dbb9bcec9a19f5d2d1c8
Juliette Cordor <grassojames5@gmail.com>
2022-11-11 16:15:42 +1100

    added time impl

--- src/bin/list.rs
+++ src/bin/list.rs
@@ -1,8 +1,15 @@
+use std::{fs::DirEntry, os::windows::prelude::MetadataExt};
+
+use chrono::{DateTime, FixedOffset, NaiveDateTime};
 use clap::Parser;
-use sfst::buckets;
+use serde::{Deserialize, Serialize};
+use sfst::buckets::{self, Bucket};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ListOutput {}
 
 #[derive(Debug, Parser)]
-pub struct ListArgs {
+struct ListArgs {
     #[clap(help = "The pattern to search for (can be a regex)")]
     pattern: Option<String>,
 
@@ -13,6 +20,29 @@ pub struct ListArgs {
     hook: bool,
 }
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     let scoop_buckets_path = buckets::get_path();
+
+    let read = scoop_buckets_path
+        .read_dir()?
+        .collect::<Result<Vec<DirEntry>, _>>()?;
+
+    let offset = {
+        let now = chrono::Local::now();
+
+        *now.offset()
+    };
+
+    for bucket in read {
+        let path = bucket.path();
+        let updated = path.metadata()?.last_write_time();
+
+        let naive_time = NaiveDateTime::from_timestamp(updated.try_into()?, 0);
+
+        let date_time = DateTime::<FixedOffset>::from_local(naive_time, offset);
+
+        let bucket = Bucket::open(path)?;
+    }
+
+    Ok(())
 }
