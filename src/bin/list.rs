2ad524e03ec08d6770762b816dd2d89fd103650d
Juliette Cordor <61040004+jewlexx@users.noreply.github.com>
2022-11-17 13:17:45 +1100

    use bucket name rather than remote

--- src/bin/list.rs
+++ src/bin/list.rs
@@ -81,12 +81,10 @@ fn main() -> anyhow::Result<()> {
 
             let install_manifest = InstallManifest::from_path(&app_current.join("install.json"))?;
 
-            let bucket = Bucket::open(install_manifest.bucket)?;
-
             anyhow::Ok(OutputPackage {
                 name: package_name.to_string(),
                 version: manifest.version,
-                source: bucket.get_remote()?,
+                source: install_manifest.bucket,
                 updated: date_time.to_rfc3339(),
             })
         })
