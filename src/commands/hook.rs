77de55ca142d869b1a5f7bf5854333aa29914f62
Juliette Cordor <grassojames5@gmail.com>
2022-12-26 00:52:04 +1100

    fixed powershell hook

--- src/commands/hook.rs
+++ src/commands/hook.rs
@@ -17,12 +17,12 @@ impl super::Command for Args {
 
         if !self.no_search {
             print!(
-                "if ($args[0] -eq 'search') {{ sfss.exe @($args | Select-Object -Skip 1) }} else"
+                "if ($args[0] -eq 'search') {{ sfsu.exe search @($args | Select-Object -Skip 1) }} else"
             );
         }
 
         if !self.no_list {
-            print!("if ($args[0] -eq 'list') {{ sfsl.exe --json @($args | Select-Object -Skip 1) | ConvertFrom-Json }} else");
+            print!("if ($args[0] -eq 'list') {{ sfsu.exe list --json @($args | Select-Object -Skip 1) | ConvertFrom-Json }} else");
         }
 
         print!(" {{ scoop.ps1 @args }} }}");
