078a7904411011c35267fd21f7ade98e279d6c41
Juliette Cordor <grassojames5@gmail.com>
2023-01-23 18:01:44 +1100

    move to gnu linker rather than msvc

--- generate-release.ps1
+++ generate-release.ps1
@@ -1,39 +1,41 @@
-function Out-Hashes($Path) {
+function Out-Hashes($Path)
+{
     $Hash = Get-FileHash $Path -Algorithm SHA256
     $Hash.Hash | Out-File "$Path.sha256"
 }
 
-cargo b -r --target x86_64-pc-windows-msvc
-cargo b -r --target i686-pc-windows-msvc
+cargo b -r --target x86_64-pc-windows-gnu
+cargo b -r --target i686-pc-windows-gnu
 
-if (Test-Path release) {
-    rm -r release
+if (Test-Path release)
+{
+    Remove-Item -r release
 }
 
 mkdir release
 mkdir release/64bit
 mkdir release/32bit
 
-cp target/x86_64-pc-windows-msvc/release/*.exe release/64bit
+Copy-Item target/x86_64-pc-windows-gnu/release/*.exe release/64bit
 
-cd release/64bit;
+Set-Location release/64bit;
 
 7z a 'dl-x86_64' '*.exe'
 
 Out-Hashes dl-x86_64.7z
 
-mv *.7z* ../
+Move-Item *.7z* ../
 
-cd ../..
+Set-Location ../..
 
-cp target/i686-pc-windows-msvc/release/*.exe release/32bit
+Copy-Item target/i686-pc-windows-gnu/release/*.exe release/32bit
 
-cd release/32bit
+Set-Location release/32bit
 
 7z a 'dl-i686' '*.exe'
 
 Out-Hashes dl-i686.7z
 
-mv *.7z* ../
+Move-Item *.7z* ../
 
-cd ../..
+Set-Location ../..
