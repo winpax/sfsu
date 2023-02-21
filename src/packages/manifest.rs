e6b168fc877771ef103bba95f033ad8907a5dc57
Juliette Cordor <grassojames5@gmail.com>
2023-02-21 15:27:33 +1100

    removed redundant pub(crate)

--- src/packages/manifest.rs
+++ src/packages/manifest.rs
@@ -8,199 +8,199 @@ use serde::{Deserialize, Serialize};
 pub struct Manifest {
     /// A comment.
     #[serde(rename = "##")]
-    pub(crate) empty: Option<StringOrArrayOfStrings>,
+    pub empty: Option<StringOrArrayOfStrings>,
     #[serde(rename = "$schema")]
-    pub(crate) schema: Option<String>,
+    pub schema: Option<String>,
     /// Deprecated. Use ## instead.
     #[serde(rename = "_comment")]
-    pub(crate) comment: Option<StringOrArrayOfStrings>,
-    pub(crate) architecture: Option<Architecture>,
-    pub(crate) autoupdate: Option<Autoupdate>,
-    pub(crate) bin: Option<StringOrArrayOfStringsOrAnArrayOfArrayOfStrings>,
-    pub(crate) checkver: Option<Checkver>,
+    pub comment: Option<StringOrArrayOfStrings>,
+    pub architecture: Option<Architecture>,
+    pub autoupdate: Option<Autoupdate>,
+    pub bin: Option<StringOrArrayOfStringsOrAnArrayOfArrayOfStrings>,
+    pub checkver: Option<Checkver>,
     /// Undocumented: Found at https://github.com/se35710/scoop-java/search?l=JSON&q=cookie
-    pub(crate) cookie: Option<HashMap<String, Option<serde_json::Value>>>,
-    pub(crate) depends: Option<StringOrArrayOfStrings>,
-    pub(crate) description: Option<String>,
-    pub(crate) env_add_path: Option<StringOrArrayOfStrings>,
-    pub(crate) env_set: Option<HashMap<String, Option<serde_json::Value>>>,
-    pub(crate) extract_dir: Option<StringOrArrayOfStrings>,
-    pub(crate) extract_to: Option<StringOrArrayOfStrings>,
-    pub(crate) hash: Option<StringOrArrayOfStrings>,
-    pub(crate) homepage: Option<String>,
+    pub cookie: Option<HashMap<String, Option<serde_json::Value>>>,
+    pub depends: Option<StringOrArrayOfStrings>,
+    pub description: Option<String>,
+    pub env_add_path: Option<StringOrArrayOfStrings>,
+    pub env_set: Option<HashMap<String, Option<serde_json::Value>>>,
+    pub extract_dir: Option<StringOrArrayOfStrings>,
+    pub extract_to: Option<StringOrArrayOfStrings>,
+    pub hash: Option<StringOrArrayOfStrings>,
+    pub homepage: Option<String>,
     /// True if the installer InnoSetup based. Found in
     /// https://github.com/ScoopInstaller/Main/search?l=JSON&q=innosetup
-    pub(crate) innosetup: Option<bool>,
-    pub(crate) installer: Option<Installer>,
-    pub(crate) license: Option<PackageLicense>,
+    pub innosetup: Option<bool>,
+    pub installer: Option<Installer>,
+    pub license: Option<PackageLicense>,
     /// Deprecated
-    pub(crate) msi: Option<StringOrArrayOfStrings>,
-    pub(crate) notes: Option<StringOrArrayOfStrings>,
-    pub(crate) persist: Option<StringOrArrayOfStringsOrAnArrayOfArrayOfStrings>,
-    pub(crate) post_install: Option<StringOrArrayOfStrings>,
-    pub(crate) post_uninstall: Option<StringOrArrayOfStrings>,
-    pub(crate) pre_install: Option<StringOrArrayOfStrings>,
-    pub(crate) pre_uninstall: Option<StringOrArrayOfStrings>,
-    pub(crate) psmodule: Option<Psmodule>,
-    pub(crate) shortcuts: Option<Vec<Vec<String>>>,
-    pub(crate) suggest: Option<Suggest>,
-    pub(crate) uninstaller: Option<Uninstaller>,
-    pub(crate) url: Option<StringOrArrayOfStrings>,
-    pub(crate) version: String,
+    pub msi: Option<StringOrArrayOfStrings>,
+    pub notes: Option<StringOrArrayOfStrings>,
+    pub persist: Option<StringOrArrayOfStringsOrAnArrayOfArrayOfStrings>,
+    pub post_install: Option<StringOrArrayOfStrings>,
+    pub post_uninstall: Option<StringOrArrayOfStrings>,
+    pub pre_install: Option<StringOrArrayOfStrings>,
+    pub pre_uninstall: Option<StringOrArrayOfStrings>,
+    pub psmodule: Option<Psmodule>,
+    pub shortcuts: Option<Vec<Vec<String>>>,
+    pub suggest: Option<Suggest>,
+    pub uninstaller: Option<Uninstaller>,
+    pub url: Option<StringOrArrayOfStrings>,
+    pub version: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Architecture {
     #[serde(rename = "32bit")]
-    pub(crate) the_32_bit: Option<The32BitClass>,
+    pub the_32_bit: Option<The32BitClass>,
     #[serde(rename = "64bit")]
-    pub(crate) the_64_bit: Option<The32BitClass>,
-    pub(crate) arm64: Option<The32BitClass>,
+    pub the_64_bit: Option<The32BitClass>,
+    pub arm64: Option<The32BitClass>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct The32BitClass {
-    pub(crate) bin: Option<StringOrArrayOfStringsOrAnArrayOfArrayOfStrings>,
-    pub(crate) checkver: Option<Checkver>,
-    pub(crate) env_add_path: Option<StringOrArrayOfStrings>,
-    pub(crate) env_set: Option<HashMap<String, Option<serde_json::Value>>>,
-    pub(crate) extract_dir: Option<StringOrArrayOfStrings>,
-    pub(crate) hash: Option<StringOrArrayOfStrings>,
-    pub(crate) installer: Option<Installer>,
+    pub bin: Option<StringOrArrayOfStringsOrAnArrayOfArrayOfStrings>,
+    pub checkver: Option<Checkver>,
+    pub env_add_path: Option<StringOrArrayOfStrings>,
+    pub env_set: Option<HashMap<String, Option<serde_json::Value>>>,
+    pub extract_dir: Option<StringOrArrayOfStrings>,
+    pub hash: Option<StringOrArrayOfStrings>,
+    pub installer: Option<Installer>,
     /// Deprecated
-    pub(crate) msi: Option<StringOrArrayOfStrings>,
-    pub(crate) post_install: Option<StringOrArrayOfStrings>,
-    pub(crate) post_uninstall: Option<StringOrArrayOfStrings>,
-    pub(crate) pre_install: Option<StringOrArrayOfStrings>,
-    pub(crate) pre_uninstall: Option<StringOrArrayOfStrings>,
-    pub(crate) shortcuts: Option<Vec<Vec<String>>>,
-    pub(crate) uninstaller: Option<Uninstaller>,
-    pub(crate) url: Option<StringOrArrayOfStrings>,
+    pub msi: Option<StringOrArrayOfStrings>,
+    pub post_install: Option<StringOrArrayOfStrings>,
+    pub post_uninstall: Option<StringOrArrayOfStrings>,
+    pub pre_install: Option<StringOrArrayOfStrings>,
+    pub pre_uninstall: Option<StringOrArrayOfStrings>,
+    pub shortcuts: Option<Vec<Vec<String>>>,
+    pub uninstaller: Option<Uninstaller>,
+    pub url: Option<StringOrArrayOfStrings>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct CheckverClass {
-    pub(crate) github: Option<String>,
+    pub github: Option<String>,
     /// Same as 'jsonpath'
-    pub(crate) jp: Option<String>,
-    pub(crate) jsonpath: Option<String>,
+    pub jp: Option<String>,
+    pub jsonpath: Option<String>,
     /// Same as 'regex'
-    pub(crate) re: Option<String>,
-    pub(crate) regex: Option<String>,
+    pub re: Option<String>,
+    pub regex: Option<String>,
     /// Allows rearrange the regexp matches
-    pub(crate) replace: Option<String>,
+    pub replace: Option<String>,
     /// Reverse the order of regex matches
-    pub(crate) reverse: Option<bool>,
+    pub reverse: Option<bool>,
     /// Custom PowerShell script to retrieve application version using more complex approach.
-    pub(crate) script: Option<StringOrArrayOfStrings>,
-    pub(crate) sourceforge: Option<SourceforgeUnion>,
-    pub(crate) url: Option<String>,
-    pub(crate) useragent: Option<String>,
-    pub(crate) xpath: Option<String>,
+    pub script: Option<StringOrArrayOfStrings>,
+    pub sourceforge: Option<SourceforgeUnion>,
+    pub url: Option<String>,
+    pub useragent: Option<String>,
+    pub xpath: Option<String>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct SourceforgeClass {
-    pub(crate) path: Option<String>,
-    pub(crate) project: Option<String>,
+    pub path: Option<String>,
+    pub project: Option<String>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Installer {
     /// Undocumented: only used in scoop-extras/oraclejdk* and scoop-extras/appengine-go
     #[serde(rename = "_comment")]
-    pub(crate) comment: Option<String>,
-    pub(crate) args: Option<StringOrArrayOfStrings>,
-    pub(crate) file: Option<String>,
-    pub(crate) keep: Option<bool>,
-    pub(crate) script: Option<StringOrArrayOfStrings>,
+    pub comment: Option<String>,
+    pub args: Option<StringOrArrayOfStrings>,
+    pub file: Option<String>,
+    pub keep: Option<bool>,
+    pub script: Option<StringOrArrayOfStrings>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Uninstaller {
-    pub(crate) args: Option<StringOrArrayOfStrings>,
-    pub(crate) file: Option<String>,
-    pub(crate) script: Option<StringOrArrayOfStrings>,
+    pub args: Option<StringOrArrayOfStrings>,
+    pub file: Option<String>,
+    pub script: Option<StringOrArrayOfStrings>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Autoupdate {
-    pub(crate) architecture: Option<AutoupdateArchitecture>,
-    pub(crate) bin: Option<StringOrArrayOfStringsOrAnArrayOfArrayOfStrings>,
-    pub(crate) env_add_path: Option<StringOrArrayOfStrings>,
-    pub(crate) env_set: Option<HashMap<String, Option<serde_json::Value>>>,
-    pub(crate) extract_dir: Option<StringOrArrayOfStrings>,
-    pub(crate) hash: Option<HashExtractionOrArrayOfHashExtractions>,
-    pub(crate) installer: Option<AutoupdateInstaller>,
-    pub(crate) license: Option<AutoupdateLicense>,
-    pub(crate) notes: Option<StringOrArrayOfStrings>,
-    pub(crate) persist: Option<StringOrArrayOfStringsOrAnArrayOfArrayOfStrings>,
-    pub(crate) psmodule: Option<AutoupdatePsmodule>,
-    pub(crate) shortcuts: Option<Vec<Vec<String>>>,
-    pub(crate) url: Option<StringOrArrayOfStrings>,
+    pub architecture: Option<AutoupdateArchitecture>,
+    pub bin: Option<StringOrArrayOfStringsOrAnArrayOfArrayOfStrings>,
+    pub env_add_path: Option<StringOrArrayOfStrings>,
+    pub env_set: Option<HashMap<String, Option<serde_json::Value>>>,
+    pub extract_dir: Option<StringOrArrayOfStrings>,
+    pub hash: Option<HashExtractionOrArrayOfHashExtractions>,
+    pub installer: Option<AutoupdateInstaller>,
+    pub license: Option<AutoupdateLicense>,
+    pub notes: Option<StringOrArrayOfStrings>,
+    pub persist: Option<StringOrArrayOfStringsOrAnArrayOfArrayOfStrings>,
+    pub psmodule: Option<AutoupdatePsmodule>,
+    pub shortcuts: Option<Vec<Vec<String>>>,
+    pub url: Option<StringOrArrayOfStrings>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct AutoupdateArchitecture {
     #[serde(rename = "32bit")]
-    pub(crate) the_32_bit: Option<AutoupdateArch>,
+    pub the_32_bit: Option<AutoupdateArch>,
     #[serde(rename = "64bit")]
-    pub(crate) the_64_bit: Option<AutoupdateArch>,
-    pub(crate) arm64: Option<AutoupdateArch>,
+    pub the_64_bit: Option<AutoupdateArch>,
+    pub arm64: Option<AutoupdateArch>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct AutoupdateArch {
-    pub(crate) bin: Option<StringOrArrayOfStringsOrAnArrayOfArrayOfStrings>,
-    pub(crate) env_add_path: Option<StringOrArrayOfStrings>,
-    pub(crate) env_set: Option<HashMap<String, Option<serde_json::Value>>>,
-    pub(crate) extract_dir: Option<StringOrArrayOfStrings>,
-    pub(crate) hash: Option<HashExtractionOrArrayOfHashExtractions>,
-    pub(crate) installer: Option<PurpleInstaller>,
-    pub(crate) shortcuts: Option<Vec<Vec<String>>>,
-    pub(crate) url: Option<StringOrArrayOfStrings>,
+    pub bin: Option<StringOrArrayOfStringsOrAnArrayOfArrayOfStrings>,
+    pub env_add_path: Option<StringOrArrayOfStrings>,
+    pub env_set: Option<HashMap<String, Option<serde_json::Value>>>,
+    pub extract_dir: Option<StringOrArrayOfStrings>,
+    pub hash: Option<HashExtractionOrArrayOfHashExtractions>,
+    pub installer: Option<PurpleInstaller>,
+    pub shortcuts: Option<Vec<Vec<String>>>,
+    pub url: Option<StringOrArrayOfStrings>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct HashExtraction {
     /// Same as 'regex'
-    pub(crate) find: Option<String>,
+    pub find: Option<String>,
     /// Same as 'jsonpath'
-    pub(crate) jp: Option<String>,
-    pub(crate) jsonpath: Option<String>,
-    pub(crate) mode: Option<Mode>,
-    pub(crate) regex: Option<String>,
+    pub jp: Option<String>,
+    pub jsonpath: Option<String>,
+    pub mode: Option<Mode>,
+    pub regex: Option<String>,
     /// Deprecated, hash type is determined automatically
     #[serde(rename = "type")]
-    pub(crate) hash_extraction_type: Option<Type>,
-    pub(crate) url: Option<String>,
-    pub(crate) xpath: Option<String>,
+    pub hash_extraction_type: Option<Type>,
+    pub url: Option<String>,
+    pub xpath: Option<String>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct PurpleInstaller {
-    pub(crate) file: Option<String>,
+    pub file: Option<String>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct AutoupdateInstaller {
-    pub(crate) file: Option<String>,
+    pub file: Option<String>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct License {
-    pub(crate) identifier: String,
-    pub(crate) url: Option<String>,
+    pub identifier: String,
+    pub url: Option<String>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct AutoupdatePsmodule {
-    pub(crate) name: Option<String>,
+    pub name: Option<String>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Psmodule {
-    pub(crate) name: Option<String>,
+    pub name: Option<String>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
