682f0aab2796efcc5135efae894e850ce8bb1363
Juliette Cordor <juliette@jewelexx.com>
2023-03-06 21:11:53 +1100

    allow user to pass shell rather than using flags

--- src/commands/hook.rs
+++ src/commands/hook.rs
@@ -1,13 +1,21 @@
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 use strum::IntoEnumIterator;
+
+#[derive(Debug, Default, ValueEnum, Copy, Clone)]
+enum Shell {
+    #[default]
+    Powershell,
+    Bash,
+    Zsh,
+}
 
 #[derive(Debug, Clone, Parser)]
 pub struct Args {
     #[clap(short = 'D', long, help = "The commands to disable")]
     disable: Vec<super::CommandsRaw>,
 
-    #[clap(short, long, help = "Print the WSL/MSYS2 version of the hooks")]
-    nix: bool,
+    #[clap(short, long, help = "Print hooks for the given shell")]
+    shell: Shell,
 }
 
 // TODO: Add function to generate hooks
@@ -20,25 +28,28 @@ impl super::Command for Args {
             .filter(|variant| !self.disable.contains(variant))
             .collect();
 
-        if self.nix {
-            println!("export SCOOP_EXEC=$(which scoop)");
+        match self.shell {
+            Shell::Powershell => {
+                print!("function scoop {{ switch ($args[0]) {{ ");
 
-            println!("scoop () {{\n  case $1 in");
+                for command in enabled_hooks {
+                    print!("'{command}' {{ return sfsu.exe $args }} ");
+                }
 
-            for command in enabled_hooks {
-                println!("      ({command}) sfsu.exe {command} $@ ;;");
+                print!("default {{ scoop.ps1 @args }} }} }}");
             }
+            Shell::Bash | Shell::Zsh => {
+                println!("export SCOOP_EXEC=$(which scoop)");
 
-            println!("      (*) $SCOOP_EXEC $@ ;;");
-            println!("  esac\n}}");
-        } else {
-            print!("function scoop {{ switch ($args[0]) {{ ");
+                println!("scoop () {{\n  case $1 in");
 
-            for command in enabled_hooks {
-                print!("'{command}' {{ return sfsu.exe $args }} ");
+                for command in enabled_hooks {
+                    println!("      ({command}) sfsu.exe {command} $@ ;;");
+                }
+
+                println!("      (*) $SCOOP_EXEC $@ ;;");
+                println!("  esac\n}}");
             }
-
-            print!("default {{ scoop.ps1 @args }} }} }}");
         }
 
         Ok(())
