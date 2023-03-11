765aed4bddfb410cf7af6eb6ce6c842cd3c01a54
Juliette Cordor <juliette@jewelexx.com>
2023-03-11 22:14:00 +1100

    change unwrap to expect

--- src/main.rs
+++ src/main.rs
@@ -41,7 +41,7 @@ fn get_scoop_path() -> PathBuf {
     // TODO: Add support for both global and non-global scoop installs
 
     let scoop_path =
-        var_os("SCOOP").map_or_else(|| dirs::home_dir().unwrap().join("scoop"), PathBuf::from);
+        var_os("SCOOP").map_or_else(|| dirs::home_dir().expect("user home directory").join("scoop"), PathBuf::from);
 
     if scoop_path.exists() {
         dunce::canonicalize(scoop_path).expect("failed to find real path to scoop")
