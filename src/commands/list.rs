337b85648f7ab77c543cfa4f7a3eefe7d8c7d0ca
Juliette Cordor <juliette@jewelexx.com>
2023-04-07 23:16:20 +1000

    moved large closures into functions

--- src/commands/list.rs
+++ src/commands/list.rs
@@ -1,4 +1,4 @@
-use std::{ffi::OsStr, time::UNIX_EPOCH};
+use std::{ffi::OsStr, fs::DirEntry, time::UNIX_EPOCH};
 
 use rayon::prelude::*;
 
@@ -34,6 +34,79 @@ pub struct Args {
     json: bool,
 }
 
+fn offset() -> FixedOffset {
+    let now = chrono::Local::now();
+
+    *now.offset()
+}
+
+fn check_lengths(
+    og: (usize, usize, usize, usize, usize),
+    pkg: &OutputPackage,
+) -> (usize, usize, usize, usize, usize) {
+    let mut new = og;
+    // Checks for the largest size out of the previous one, the current one and the section title
+    new.0 = *["Name".len(), pkg.name.len(), new.0].iter().max().unwrap();
+    new.1 = *["Version".len(), pkg.version.len(), new.1]
+        .iter()
+        .max()
+        .unwrap();
+    new.2 = *["Source".len(), pkg.source.len(), new.2]
+        .iter()
+        .max()
+        .unwrap();
+    new.3 = *["Updated".len(), pkg.updated.len(), new.3]
+        .iter()
+        .max()
+        .unwrap();
+    new.4 = *["Notes".len(), pkg.notes.len(), new.4]
+        .iter()
+        .max()
+        .unwrap();
+
+    new
+}
+
+fn parse_package(package: &DirEntry) -> anyhow::Result<OutputPackage> {
+    let path = dunce::realpath(package.path())?;
+    let updated_sys = path.metadata()?.modified()?;
+    let updated = updated_sys.duration_since(UNIX_EPOCH)?.as_secs();
+
+    let package_name = path
+        .components()
+        .last()
+        .unwrap()
+        .as_os_str()
+        .to_string_lossy();
+
+    let naive_time = {
+        let secs = updated.try_into()?;
+
+        NaiveDateTime::from_timestamp_opt(secs, 0).expect("invalid or out-of-range datetime")
+    };
+
+    let date_time = DateTime::<FixedOffset>::from_local(naive_time, offset());
+
+    let app_current = path.join("current");
+
+    let manifest = Manifest::from_path(app_current.join("manifest.json")).unwrap_or_default();
+
+    let install_manifest =
+        InstallManifest::from_path(app_current.join("install.json")).unwrap_or_default();
+
+    anyhow::Ok(OutputPackage {
+        name: package_name.to_string(),
+        version: manifest.version,
+        source: install_manifest.get_source(),
+        updated: date_time.to_rfc3339(),
+        notes: if install_manifest.hold.is_true() {
+            String::from("Hold")
+        } else {
+            String::new()
+        },
+    })
+}
+
 impl super::Command for Args {
     type Error = anyhow::Error;
 
@@ -42,57 +115,11 @@ impl super::Command for Args {
 
         let read = scoop_apps_path.read_dir()?.collect::<Result<Vec<_>, _>>()?;
 
-        let offset = {
-            let now = chrono::Local::now();
-
-            *now.offset()
-        };
-
         let outputs = read
             .par_iter()
             // We cannot search the scoop app as it is built in and hence doesn't contain any manifest
             .filter(|package| package.path().iter().last() != Some(OsStr::new("scoop")))
-            .map(|package| {
-                let path = dunce::realpath(package.path())?;
-                let updated_sys = path.metadata()?.modified()?;
-                let updated = updated_sys.duration_since(UNIX_EPOCH)?.as_secs();
-
-                let package_name = path
-                    .components()
-                    .last()
-                    .unwrap()
-                    .as_os_str()
-                    .to_string_lossy();
-
-                let naive_time = {
-                    let secs = updated.try_into()?;
-
-                    NaiveDateTime::from_timestamp_opt(secs, 0)
-                        .expect("invalid or out-of-range datetime")
-                };
-
-                let date_time = DateTime::<FixedOffset>::from_local(naive_time, offset);
-
-                let app_current = path.join("current");
-
-                let manifest =
-                    Manifest::from_path(app_current.join("manifest.json")).unwrap_or_default();
-
-                let install_manifest = InstallManifest::from_path(app_current.join("install.json"))
-                    .unwrap_or_default();
-
-                anyhow::Ok(OutputPackage {
-                    name: package_name.to_string(),
-                    version: manifest.version,
-                    source: install_manifest.get_source(),
-                    updated: date_time.to_rfc3339(),
-                    notes: if install_manifest.hold.is_true() {
-                        String::from("Hold")
-                    } else {
-                        String::new()
-                    },
-                })
-            })
+            .map(parse_package)
             .filter(|package| {
                 if let Ok(pkg) = package {
                     if let Some(ref bucket) = self.bucket {
@@ -104,17 +131,7 @@ impl super::Command for Args {
             })
             .collect::<Result<Vec<_>, _>>()?;
 
-        let max_lengths = outputs.iter().fold((0, 0, 0, 0, 0), |mut og, pkg| {
-            use std::cmp;
-
-            og.0 = cmp::max(pkg.name.len(), og.0);
-            og.1 = cmp::max(pkg.version.len(), og.1);
-            og.2 = cmp::max(pkg.source.len(), og.2);
-            og.3 = cmp::max(pkg.updated.len(), og.3);
-            og.4 = cmp::max(pkg.notes.len(), og.4);
-
-            og
-        });
+        let max_lengths = outputs.iter().fold((0, 0, 0, 0, 0), check_lengths);
 
         if self.json {
             let output_json = serde_json::to_string_pretty(&outputs)?;
