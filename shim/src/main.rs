529494525fd23a01cf8d542a6da45157929d5595
Juliette Cordor <juliette@jewelexx.com>
2023-10-03 11:11:28 +1100

    fixes

--- shim/src/main.rs
+++ shim/src/main.rs
@@ -1,4 +1,4 @@
-use std::ffi::OsString;
+use std::{ffi::OsString, path::PathBuf};
 
 use windows::{
     core::PCWSTR,
@@ -42,35 +42,9 @@ const MAX_FILENAME_SIZE: usize = 512;
 //   }
 // }
 
-struct FileName {
-    file_name: [u16; MAX_FILENAME_SIZE + 2],
-}
-
-impl FileName {
-    pub fn load() -> Self {
-        let mut file_name = [0; MAX_FILENAME_SIZE + 2];
-
-        unsafe {
-            GetModuleFileNameW(HMODULE(0), &mut file_name);
-        }
-
-        Self { file_name }
-    }
-}
-
-impl std::fmt::Display for FileName {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let string_lossy = String::from_utf16_lossy(&self.file_name);
-        let trimmed = string_lossy.trim_end_matches('\0');
-
-        write!(f, "{trimmed}")
-    }
-}
-
 fn main() {
     let command_line: PCWSTR = unsafe { GetCommandLineW() };
-    let file_name = FileName::load();
-    dbg!(file_name.to_string());
+    let file_name = std::env::current_exe().unwrap();
 
     println!("Hello, world!");
 }
