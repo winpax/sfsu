1b8494eb921abce4795e9ef8ca9900537286af05
Juliette Cordor <juliette@jewelexx.com>
2023-10-14 12:36:17 +1100

    rename proc macro

--- sfsu-derive/src/lib.rs
+++ sfsu-derive/src/lib.rs
@@ -11,8 +11,8 @@ pub fn derive_into_inner(input: TokenStream) -> TokenStream {
     inner::into_inner(parse_macro_input!(input as DeriveInput)).into()
 }
 
-#[proc_macro_derive(RawEnum)]
+#[proc_macro_derive(Hooks)]
 #[proc_macro_error]
-pub fn derive_raw_enum(input: TokenStream) -> TokenStream {
+pub fn derive_hook_enum(input: TokenStream) -> TokenStream {
     hooks::hook_enum(parse_macro_input!(input as DeriveInput)).into()
 }
--- sfsu-derive/tests/raw_enum.rs
+++ sfsu-derive/tests/raw_enum.rs
@@ -2,7 +2,7 @@
 
 struct DummyStruct;
 
-#[derive(sfsu_derive::RawEnum)]
+#[derive(sfsu_derive::derive_hook_enum)]
 enum EnumWithData {
     Test1(DummyStruct),
     Test2(DummyStruct),
