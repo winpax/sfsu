e0aad0cb25fe543475a8175d64869a31a0c8ca0f
Juliette Cordor <juliette@jewelexx.com>
2023-11-17 15:10:19 +1100

    move unused buckets into buckets subcommand

---
+++ src/commands/buckets.rs
@@ -0,0 +1,27 @@
+pub mod unused;
+
+use clap::{Parser, Subcommand};
+
+use sfsu_derive::{Hooks, Runnable};
+
+pub trait Command {
+    fn run(self) -> Result<(), anyhow::Error>;
+}
+
+#[derive(Debug, Hooks, Clone, Subcommand, Runnable)]
+pub enum Commands {
+    Unused(unused::Args),
+}
+
+#[derive(Debug, Clone, Parser)]
+pub struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+impl super::Command for Args {
+    #[inline]
+    fn run(self) -> Result<(), anyhow::Error> {
+        self.command.run()
+    }
+}
