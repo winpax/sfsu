4563ffddb0082ee8aae0d48e75c4b55d686a0d98
Juliette Cordor <juliette@jewelexx.com>
2023-11-24 17:14:08 +1100

    updated email

--- src/logging/panics.rs
+++ src/logging/panics.rs
@@ -4,7 +4,7 @@ pub fn handle() {
     setup_panic!(Metadata {
         name: env!("CARGO_PKG_NAME").into(),
         version: env!("CARGO_PKG_VERSION").into(),
-        authors: "Juliette Cordor <public@jewelexx.com>".into(),
+        authors: "Juliette Cordor <support+sfsu@maybejules.com>".into(),
         homepage: "github.com/jewelexx/sfsu".into(),
     });
 }
