c00be385fd83f0f1e7b14c04b45d385f6363a667
Juliette Cordor <juliette@jewelexx.com>
2023-12-23 13:47:31 +1100

    removed unused dependencies

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,6 @@ mod commands;
 mod logging;
 mod opt;
 
-use std::{mem::MaybeUninit, path::Path};
-
 use clap::Parser;
 
 use commands::Commands;
@@ -34,58 +32,58 @@ fn main() -> anyhow::Result<()> {
     args.command.run()
 }
 
-/// Get the owner of a file path
-///
-/// # Errors
-/// - Interacting with system I/O
-///
-/// # Panics
-/// - Owner's name isn't valid utf8
-#[deprecated(note = "Doesn't work properly, and I have no current plans to fix it ")]
-pub fn file_owner(path: impl AsRef<Path>) -> std::io::Result<String> {
-    use std::{fs::File, os::windows::io::AsRawHandle};
-    use windows::{
-        core::{PCSTR, PSTR},
-        Win32::{
-            Foundation::{HANDLE, PSID},
-            Security::{
-                Authorization::{GetSecurityInfo, SE_FILE_OBJECT},
-                LookupAccountSidA, OWNER_SECURITY_INFORMATION,
-            },
-        },
-    };
+// /// Get the owner of a file path
+// ///
+// /// # Errors
+// /// - Interacting with system I/O
+// ///
+// /// # Panics
+// /// - Owner's name isn't valid utf8
+// NOTE: This currently does now work
+// pub fn file_owner(path: impl AsRef<Path>) -> std::io::Result<String> {
+//     use std::{fs::File, os::windows::io::AsRawHandle};
+//     use windows::{
+//         core::{PCSTR, PSTR},
+//         Win32::{
+//             Foundation::{HANDLE, PSID},
+//             Security::{
+//                 Authorization::{GetSecurityInfo, SE_FILE_OBJECT},
+//                 LookupAccountSidA, OWNER_SECURITY_INFORMATION,
+//             },
+//         },
+//     };
 
-    let file = File::open(path.as_ref().join("current/install.json"))?;
-    let handle = HANDLE(file.as_raw_handle() as isize);
+//     let file = File::open(path.as_ref().join("current/install.json"))?;
+//     let handle = HANDLE(file.as_raw_handle() as isize);
 
-    let owner_psid: MaybeUninit<PSID> = MaybeUninit::uninit();
+//     let owner_psid: MaybeUninit<PSID> = MaybeUninit::uninit();
 
-    unsafe {
-        GetSecurityInfo(
-            handle,
-            SE_FILE_OBJECT,
-            OWNER_SECURITY_INFORMATION,
-            Some(owner_psid.as_ptr().cast_mut()),
-            None,
-            None,
-            None,
-            None,
-        )?;
-    }
+//     unsafe {
+//         GetSecurityInfo(
+//             handle,
+//             SE_FILE_OBJECT,
+//             OWNER_SECURITY_INFORMATION,
+//             Some(owner_psid.as_ptr().cast_mut()),
+//             None,
+//             None,
+//             None,
+//             None,
+//         )?;
+//     }
 
-    let owner_name = PSTR::null();
+//     let owner_name = PSTR::null();
 
-    unsafe {
-        LookupAccountSidA(
-            PCSTR::null(),
-            owner_psid.assume_init(),
-            owner_name,
-            std::ptr::null_mut(),
-            PSTR::null(),
-            std::ptr::null_mut(),
-            std::ptr::null_mut(),
-        )?;
-    }
+//     unsafe {
+//         LookupAccountSidA(
+//             PCSTR::null(),
+//             owner_psid.assume_init(),
+//             owner_name,
+//             std::ptr::null_mut(),
+//             PSTR::null(),
+//             std::ptr::null_mut(),
+//             std::ptr::null_mut(),
+//         )?;
+//     }
 
-    Ok(unsafe { owner_name.to_string().expect("valid utf8 name") })
-}
+//     Ok(unsafe { owner_name.to_string().expect("valid utf8 name") })
+// }
