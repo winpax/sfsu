72b31e43e2e4de68c309965bbb552ff66b2bee24
Juliette Cordor <professional@maybejules.com>
2024-02-24 19:09:38 +1100

    split outdated info into seperate lib file

---
+++ src/packages/outdated.rs
@@ -0,0 +1,29 @@
+use serde::Serialize;
+
+use super::Manifest;
+
+#[derive(Debug, Clone, Serialize, PartialEq, Eq, Hash)]
+#[serde(rename_all = "PascalCase")]
+pub struct Info {
+    pub name: String,
+    pub current: String,
+    pub available: String,
+}
+
+impl Info {
+    /// Get the outdated info from a local and remote manifest combo
+    ///
+    /// Returns [`None`] if they have the same version
+    #[must_use]
+    pub fn from_manifests(local: &Manifest, remote: &Manifest) -> Option<Self> {
+        if local.version == remote.version {
+            None
+        } else {
+            Some(Info {
+                name: local.name.clone(),
+                current: local.version.clone(),
+                available: remote.version.clone(),
+            })
+        }
+    }
+}
