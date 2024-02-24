7ba813bd0900899c2d2a176c9392ad56f7537524
Juliette Cordor <professional@maybejules.com>
2024-02-24 20:58:35 +1100

    removed dbg! statement

--- src/commands/info.rs
+++ src/commands/info.rs
@@ -115,7 +115,7 @@ impl super::Command for Args {
                 };
 
                 let author_wrapped =
-                    Author::from_signature(author).with_show_emails(!dbg!(self.hide_emails));
+                    Author::from_signature(author).with_show_emails(!self.hide_emails);
 
                 (
                     Some(date_time.to_string()),
