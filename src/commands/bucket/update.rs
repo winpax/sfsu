c97e35fbe986a58c2541b4da2f7660ea0923d53d
Juliette Cordor <professional@maybejules.com>
2024-03-31 23:11:54 +1100

    added todo comment to update

--- src/commands/bucket/update.rs
+++ src/commands/bucket/update.rs
@@ -18,6 +18,8 @@ pub struct Args;
 
 impl commands::Command for Args {
     fn runner(self) -> Result<(), anyhow::Error> {
+        // TODO: Add update time to bucket so scoop doesn't do it again
+
         const FINISH_MESSAGE: &str = "✅";
 
         let buckets = Bucket::list_all()?;
