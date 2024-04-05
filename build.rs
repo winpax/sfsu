8d11c2325199deee1e0d05b2a92fc2444ff8eaf3
Juliette Cordor <professional@maybejules.com>
2024-04-05 23:19:43 +1100

    remove cc build from build script

--- build.rs
+++ build.rs
@@ -1,10 +1,6 @@
 const WIN_MANIFEST: &str = include_str!("./sfsu.exe.manifest");
 
 fn main() -> shadow_rs::SdResult<()> {
-    cc::Build::new()
-        .file("src/win/version.c")
-        .compile("win_version");
-
     let shadow_res = shadow_rs::new();
 
     let mut res = winres::WindowsResource::new();
