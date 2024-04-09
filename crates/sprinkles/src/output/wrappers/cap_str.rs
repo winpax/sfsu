ff8cdd143228f5a9a92dee3b49757cc8612c770c
Juliette Cordor <professional@maybejules.com>
2024-04-09 17:58:53 +1000

    removed wrap_bool and wrap_str

--- crates/sprinkles/src/output/wrappers/cap_str.rs
+++ crates/sprinkles/src/output/wrappers/cap_str.rs
@@ -1,14 +1,5 @@
 use std::fmt::Display;
 
-#[macro_export]
-macro_rules! wrap_str {
-    ($v:literal) => {
-        CapitalizedStr::new($v)
-    };
-}
-
-pub use wrap_str;
-
 #[derive(Debug, Clone)]
 #[must_use = "Lazy. Does nothing until consumed"]
 pub struct CapitalizedStr<T>(T);
