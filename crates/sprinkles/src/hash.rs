1a8195938d886bbffdf1b1bb6457751c892f7e72
Juliette Cordor <professional@maybejules.com>
2024-04-24 19:04:09 +1000

    removed dbg! statements

--- crates/sprinkles/src/hash.rs
+++ crates/sprinkles/src/hash.rs
@@ -382,7 +382,7 @@ impl Hash {
 
         let hash = match hash_mode {
             HashMode::Extract(regex) => Hash::from_text(source.text()?, &submap, regex),
-            HashMode::Xpath(xpath) => dbg!(Hash::find_hash_in_xml(source.text()?, &submap, xpath)),
+            HashMode::Xpath(xpath) => Hash::find_hash_in_xml(source.text()?, &submap, xpath),
             HashMode::Json(json_path) => Hash::from_json(source.bytes()?, &submap, json_path),
             HashMode::Rdf => Hash::from_rdf(source.bytes()?, url.remote_filename()),
             _ => unreachable!(),
