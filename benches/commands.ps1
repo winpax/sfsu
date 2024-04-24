fa29dfb8a3cc39d9894e1f42fe430fbc398500b0
Juliette Cordor <professional@maybejules.com>
2024-04-24 14:37:14 +1000

    added prints for benchmarks

--- benches/commands.ps1
+++ benches/commands.ps1
@@ -30,17 +30,21 @@ scoop config scoop_branch develop
 pwsh -NoProfile -C 'scoop update'
 
 # Listing
+Write-Output 'Benchmarking listing commands'
 BenchCommands @('sfsu list', 'hok list', 'scoop list')
 
 # Searching
 ## Without sqlite cache
+Write-Output 'Benchmarking search without cache'
 scoop config use_sqlite_cache false
 BenchCommands @('sfsu search google', 'hok search google', 'scoop-search google', 'scoop search google')
 
 ## With sqlite cache
 scoop config use_sqlite_cache true
+Write-Output 'Benchmarking search with cache'
 BenchCommands @('sfsu search google', 'scoop search google') -OutFile 'search_cached'
 
 # Info
+Write-Output 'Benchmarking info'
 BenchCommands @('sfsu info sfsu --disable-updated', 'hok info sfsu')
 BenchCommands @('sfsu info sfsu', 'scoop info sfsu')
