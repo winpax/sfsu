b43aac4af141ee7bf086082b0260023905f4b4b2
Juliette Cordor <professional@maybejules.com>
2024-04-27 14:04:16 +1000

    implemented nicer cache entry

--- src/commands/cache.rs
+++ src/commands/cache.rs
@@ -1,12 +1,22 @@
 use std::os::windows::fs::MetadataExt;
 
+use anyhow::Context;
 use clap::Parser;
+use serde::Serialize;
 use sprinkles::{output::wrappers::sizes::Size, Scoop};
 
 #[derive(Debug, Clone, Parser)]
 pub struct Args {
     #[clap(from_global)]
     json: bool,
+}
+
+#[derive(Debug, Clone, Serialize)]
+struct CacheEntry {
+    name: String,
+    version: String,
+    url: String,
+    size: Size,
 }
 
 impl super::Command for Args {
@@ -18,8 +28,22 @@ impl super::Command for Args {
         while let Some(entry) = dir.next_entry().await? {
             let metadata = entry.metadata().await?;
 
-            let name = entry.file_name();
-            let name = name.to_string_lossy();
+            let cache_entry = {
+                let name = entry.file_name();
+                let mut parts = name.to_string_lossy().split('#');
+
+                let name = parts.next().context("No name")?;
+                let version = parts.next().context("No version")?;
+                let url = parts.next().context("No url")?;
+                let size = Size::new(metadata.file_size() as f64);
+
+                CacheEntry {
+                    name: name.to_string(),
+                    version: version.to_string(),
+                    url: url.to_string(),
+                    size,
+                }
+            };
 
             #[allow(clippy::cast_precision_loss)]
             let size = Size::new(metadata.file_size() as f64);
