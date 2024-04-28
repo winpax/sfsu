afbc77a5eb1455d1d42eee41e1ac44806ce83e4f
Juliette Cordor <professional@maybejules.com>
2024-04-28 20:39:47 +1000

    localize match paths function

--- src/commands/cache.rs
+++ src/commands/cache.rs
@@ -1,10 +1,90 @@
+use std::{os::windows::fs::MetadataExt, path::PathBuf};
+
+use anyhow::Context;
 use clap::{Parser, Subcommand};
+use regex::Regex;
+use serde::Serialize;
 use sfsu_derive::Runnable;
+use sprinkles::{output::wrappers::sizes::Size, Scoop};
+use tokio::task::JoinSet;
 
 mod rm;
 mod show;
 
 use crate::commands::Command;
+
+#[derive(Debug, Clone, Serialize, PartialEq, Eq, PartialOrd, Ord)]
+struct CacheEntry {
+    #[serde(skip)]
+    file_path: PathBuf,
+    name: String,
+    version: String,
+    size: Size,
+    url: String,
+}
+
+impl CacheEntry {
+    pub async fn match_paths(patterns: &[String]) -> anyhow::Result<Vec<Self>> {
+        let cache_path = Scoop::cache_path();
+
+        let patterns = patterns
+            .iter()
+            .filter_map(|pattern| Regex::new(&format!("^{pattern}#")).ok())
+            .collect::<Vec<_>>();
+
+        let mut set = JoinSet::new();
+        let mut dir = tokio::fs::read_dir(cache_path).await?;
+
+        while let Some(entry) = dir.next_entry().await? {
+            let file_name = entry.file_name();
+            let file_name = file_name.to_string_lossy();
+
+            if !patterns.iter().any(|pattern| pattern.is_match(&file_name)) {
+                continue;
+            }
+
+            let file_name = file_name.to_string();
+
+            set.spawn(async move {
+                let metadata = entry.metadata().await?;
+
+                let mut parts = file_name.split('#');
+
+                let name = parts.next().context("No name")?;
+                let version = parts.next().context("No version")?;
+                let url = parts.next().context("No url")?;
+
+                #[allow(clippy::cast_precision_loss)]
+                let size = Size::new(metadata.file_size());
+
+                let cache_entry = CacheEntry {
+                    file_path: entry.path(),
+                    name: name.to_string(),
+                    version: version.to_string(),
+                    url: url.to_string(),
+                    size,
+                };
+
+                anyhow::Ok(cache_entry)
+            });
+        }
+
+        let mut cache_entries = {
+            let mut cache_entries = vec![];
+
+            while let Some(result) = set.join_next().await {
+                let result = result??;
+                cache_entries.push(result);
+            }
+
+            cache_entries
+        };
+
+        cache_entries.sort();
+
+        Ok(cache_entries)
+    }
+}
 
 #[derive(Debug, Clone, Subcommand, Runnable)]
 enum Commands {
--- src/commands/cache/show.rs
+++ src/commands/cache/show.rs
@@ -1,25 +1,11 @@
-use std::os::windows::fs::MetadataExt;
 
-use anyhow::Context;
 use clap::Parser;
-use regex::Regex;
-use serde::Serialize;
 use sprinkles::{
     abandon,
-    output::{structured::Structured, wrappers::sizes::Size},
-    Scoop,
+    output::structured::Structured,
 };
-use tokio::task::JoinSet;
 
-use crate::commands::Command;
-
-#[derive(Debug, Clone, Serialize, PartialEq, Eq, PartialOrd, Ord)]
-struct CacheEntry {
-    name: String,
-    version: String,
-    size: Size,
-    url: String,
-}
+use crate::commands::{cache::CacheEntry, Command};
 
 #[derive(Debug, Clone, Parser)]
 pub struct Args {
@@ -32,66 +18,11 @@ pub struct Args {
 
 impl Command for Args {
     async fn runner(self) -> Result<(), anyhow::Error> {
-        let cache_path = Scoop::cache_path();
-
-        let patterns = self
-            .apps
-            .into_iter()
-            .filter_map(|pattern| Regex::new(&format!("^{pattern}#")).ok())
-            .collect::<Vec<_>>();
-
-        let mut set = JoinSet::new();
-        let mut dir = tokio::fs::read_dir(cache_path).await?;
-
-        while let Some(entry) = dir.next_entry().await? {
-            let file_name = entry.file_name();
-            let file_name = file_name.to_string_lossy();
-
-            if !patterns.iter().any(|pattern| pattern.is_match(&file_name)) {
-                continue;
-            }
-
-            let file_name = file_name.to_string();
-
-            set.spawn(async move {
-                let metadata = entry.metadata().await?;
-
-                let mut parts = file_name.split('#');
-
-                let name = parts.next().context("No name")?;
-                let version = parts.next().context("No version")?;
-                let url = parts.next().context("No url")?;
-
-                #[allow(clippy::cast_precision_loss)]
-                let size = Size::new(metadata.file_size());
-
-                let cache_entry = CacheEntry {
-                    name: name.to_string(),
-                    version: version.to_string(),
-                    url: url.to_string(),
-                    size,
-                };
-
-                anyhow::Ok(cache_entry)
-            });
-        }
-
-        let mut cache_entries = {
-            let mut cache_entries = vec![];
-
-            while let Some(result) = set.join_next().await {
-                let result = result??;
-                cache_entries.push(result);
-            }
-
-            cache_entries
-        };
+        let cache_entries = CacheEntry::match_paths(&self.apps).await?;
 
         if cache_entries.is_empty() {
             abandon!("No cache entries found");
         }
-
-        cache_entries.sort();
 
         let values = cache_entries
             .into_iter()
