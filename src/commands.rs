8febfdd027d91d548440138d2e4575fd4c4cfb17
Juliette Cordor <professional@maybejules.com>
2024-04-28 22:50:06 +1000

    lock cache command behind feature flag

--- src/commands.rs
+++ src/commands.rs
@@ -128,6 +128,7 @@ pub enum Commands {
     Export(export::Args),
     /// Check for common issues
     Checkup(checkup::Args),
+    #[cfg(feature = "download")]
     /// Show or clear the download cache
     Cache(cache::Args),
 }
