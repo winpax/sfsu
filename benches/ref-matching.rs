bd0a15869f1b1340b6cf73b563d0223ab9a8c7d5
Juliette Cordor <professional@maybejules.com>
2024-04-28 21:52:24 +1000

    added version matching

--- benches/ref-matching.rs
+++ benches/ref-matching.rs
@@ -1,12 +1,25 @@
+use std::str::FromStr;
+
 use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
 
-use sfsu::packages::reference::Package;
+use sprinkles::packages::reference::Package;
 
 fn criterion_benchmark(c: &mut Criterion) {
+    let runtime = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+
     c.bench_with_input(
         BenchmarkId::new("find package across buckets", "sfsu"),
-        &Package::Name("sfsu".into()),
-        |b, package| b.iter(|| black_box(package.first().unwrap())),
+        &Package::from_str("sfsu").unwrap(),
+        |b, package| b.to_async(&runtime).iter(|| black_box(package.manifest())),
+    );
+
+    c.bench_with_input(
+        BenchmarkId::new("find package with version across buckets", "sfsu@1.10.0"),
+        &Package::from_str("sfsu@1.10.0").unwrap(),
+        |b, package| b.to_async(&runtime).iter(|| black_box(package.manifest())),
     );
 }
 
