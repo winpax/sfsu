ee03da287e40f030720e1490187e295e049d0aad
Juliette Cordor <professional@maybejules.com>
2024-05-06 01:46:43 +1000

    added panic to test CI

--- build.rs
+++ build.rs
@@ -43,6 +43,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
         std::fs::write(out_path.clone() + "/contributors.rs", contributors_output)?;
     } else {
+        panic!("Missing github api key");
         std::fs::write(
             out_path.clone() + "/contributors.rs",
             "pub const CONTRIBUTORS: [(&str, &str); 0] = [];",
