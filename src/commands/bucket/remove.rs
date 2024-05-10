bd72c280159094fe745fe74f3adc45ba55b01807
Juliette Cordor <professional@maybejules.com>
2024-05-10 20:55:48 +1000

    feat: implemented bucket removal

--- src/commands/bucket/remove.rs
+++ src/commands/bucket/remove.rs
@@ -1,14 +1,49 @@
 use clap::Parser;
+use dialoguer::Confirm;
 use sprinkles::{config, contexts::ScoopContext};
+
+use crate::{abandon, output::colours::yellow};
 
 #[derive(Debug, Clone, Parser)]
 pub struct Args {
     #[clap(help = "The name of the bucket to delete")]
     name: String,
+
+    #[clap(short = 'y', long, help = "Assume \"yes\" as answer to prompts")]
+    assume_yes: bool,
 }
 
 impl super::Command for Args {
-    async fn runner(self, _ctx: &impl ScoopContext<config::Scoop>) -> anyhow::Result<()> {
+    async fn runner(self, ctx: &impl ScoopContext<config::Scoop>) -> anyhow::Result<()> {
+        let path = ctx.buckets_path().join(&self.name);
+
+        if !path.exists() {
+            abandon!("Bucket \"{}\" is not installed", self.name);
+        }
+
+        if self.name == "main" && !self.assume_yes {
+            Confirm::new()
+            .with_prompt(yellow!("You probably don't want to delete the main bucket. Are you sure you want to continue?").to_string())
+            .default(false)
+            .interact()?;
+        }
+
+        let response = if self.assume_yes {
+            true
+        } else {
+            Confirm::new()
+                .with_prompt(format!(
+                    "Are you sure you want to delete \"{}\"?",
+                    path.display()
+                ))
+                .default(false)
+                .interact()?
+        };
+
+        if response {
+            std::fs::remove_dir_all(path)?;
+        }
+
         Ok(())
     }
 }
