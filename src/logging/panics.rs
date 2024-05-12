18d161feb2321b38859259301f71132cae1a2418
Juliette Cordor <professional@maybejules.com>
2024-05-12 21:06:53 +1000

    fix: update references

--- src/logging/panics.rs
+++ src/logging/panics.rs
@@ -5,6 +5,6 @@ pub fn handle() {
         Metadata::new(env!("CARGO_PKG_NAME"), crate::versions::SFSU_LONG_VERSION)
             .authors(env!("CARGO_PKG_AUTHORS").replace(':', ", "))
             .homepage(env!("CARGO_PKG_HOMEPAGE"))
-            .support("Open an issue on GitHub: https://github.com/jewlexx/sfsu/issues/new, and upload the aforementioned report file.")
+            .support("Open an issue on GitHub: https://github.com/winpax/sfsu/issues/new, and upload the aforementioned report file.")
     };
 }
