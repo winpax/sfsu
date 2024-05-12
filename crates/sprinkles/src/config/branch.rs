67dd777f743a47f30841c629058506a6ae6d1e8e
Juliette Cordor <professional@maybejules.com>
2024-05-12 11:46:11 +1000

    fix: custom branch in scoop config

--- crates/sprinkles/src/config/branch.rs
+++ crates/sprinkles/src/config/branch.rs
@@ -4,8 +4,7 @@ use std::fmt::Display;
 
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Default, Copy, Clone, PartialEq, Eq, Serialize, Deserialize)]
-#[serde(rename_all = "lowercase")]
+#[derive(Debug, Default, Clone, PartialEq, Eq)]
 /// Which Scoop branch to use
 pub enum ScoopBranch {
     #[default]
@@ -13,15 +12,42 @@ pub enum ScoopBranch {
     Master,
     /// The develop branch
     Develop,
+    /// A custom branch
+    Other(String),
+}
+
+impl Serialize for ScoopBranch {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_str(self.name())
+    }
+}
+
+impl<'de> Deserialize<'de> for ScoopBranch {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+
+        match s.as_str() {
+            "master" => Ok(Self::Master),
+            "develop" => Ok(Self::Develop),
+            _ => Ok(Self::Other(s)),
+        }
+    }
 }
 
 impl ScoopBranch {
     #[must_use]
     /// Get the branch name
-    pub fn name(self) -> &'static str {
+    pub fn name(&self) -> &str {
         match self {
             ScoopBranch::Master => "master",
             ScoopBranch::Develop => "develop",
+            Self::Other(branch) => branch.as_str(),
         }
     }
 }
