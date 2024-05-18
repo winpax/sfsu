5c133ac9bd0bd3dc3ce68de517a224f293188e71
Juliette Cordor <professional@maybejules.com>
2024-05-19 02:00:40 +1000

    feat: add shim delete flags

--- crates/sprinkles/src/packages/handles/shim.rs
+++ crates/sprinkles/src/packages/handles/shim.rs
@@ -2,6 +2,56 @@
 
 use std::path::{Path, PathBuf};
 
+#[derive(Debug, thiserror::Error)]
+#[allow(missing_docs)]
+/// Shim errors
+pub enum Error {
+    #[error("Deleting shims: {0}")]
+    IOError(#[from] std::io::Error),
+}
+
+/// Shim result type
+pub type Result<T, E = Error> = std::result::Result<T, E>;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+/// Flags for deleting shims
+pub struct DeleteFlags(u8);
+
+impl DeleteFlags {
+    /// Delete just the executable
+    pub const EXECUTABLE: Self = Self(1);
+    /// Delete just the shim
+    pub const SHIM: Self = Self(1 >> 1);
+
+    #[must_use]
+    /// Check if we should delete the executable
+    pub fn is_executable(self) -> bool {
+        self & Self::EXECUTABLE == Self::EXECUTABLE
+    }
+
+    #[must_use]
+    /// Check if we should delete the shim
+    pub fn is_shim(self) -> bool {
+        self & Self::SHIM == Self::SHIM
+    }
+}
+
+impl std::ops::BitOr for DeleteFlags {
+    type Output = Self;
+
+    fn bitor(self, rhs: Self) -> Self::Output {
+        Self(self.0 | rhs.0)
+    }
+}
+
+impl std::ops::BitAnd for DeleteFlags {
+    type Output = Self;
+
+    fn bitand(self, rhs: Self) -> Self::Output {
+        Self(self.0 & rhs.0)
+    }
+}
+
 #[derive(Debug, Clone, PartialEq, Eq)]
 /// A shim handle
 pub struct ShimHandle {
@@ -38,5 +88,35 @@ impl ShimHandle {
         } else {
             None
         }
+    }
+
+    /// Delete the shim and executable
+    ///
+    /// # Errors
+    /// - Deleting the shim failed
+    /// - Deleting the executable failed
+    pub fn delete_all(&self) -> Result<()> {
+        self.delete(DeleteFlags::EXECUTABLE | DeleteFlags::SHIM)
+    }
+
+    /// Delete the shim and/or the executable
+    ///
+    /// # Errors
+    /// - Deleting the shim failed
+    /// - Deleting the executable failed
+    pub fn delete(&self, flags: DeleteFlags) -> Result<()> {
+        if flags.is_executable() {
+            if let Some(executable) = self.executable() {
+                std::fs::remove_file(executable)?;
+            }
+        }
+
+        if flags.is_shim() {
+            if let Some(shim) = self.shim() {
+                std::fs::remove_file(shim)?;
+            }
+        }
+
+        Ok(())
     }
 }
