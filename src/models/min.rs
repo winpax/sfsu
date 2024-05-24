9a5cd1c173626401ea47d521e05d086dcb2b541f
Juliette Cordor <professional@maybejules.com>
2024-05-24 16:44:13 +1000

    chore: add MinInfo struct to sfsu project

---
+++ src/models/min.rs
@@ -0,0 +1,122 @@
+use std::path::Path;
+use std::time::UNIX_EPOCH;
+use anyhow::Context;
+use chrono::{DateTime, Local};
+use quork::traits::truthy::ContainsTruth;
+use serde::Serialize;
+use sprinkles::{
+    config,
+    contexts::ScoopContext,
+    packages::{CreateManifest, InstallManifest, Manifest},
+    wrappers::time::NicerTime,
+};
+use rayon::prelude::*;
+
+#[derive(Debug, Serialize)]
+/// Minimal package info
+pub struct MinInfo {
+    /// The name of the package
+    pub name: String,
+    /// The version of the package
+    pub version: String,
+    /// The package's source (eg. bucket name)
+    pub source: String,
+    /// The last time the package was updated
+    pub updated: NicerTime<Local>,
+    /// The package's notes
+    pub notes: String,
+}
+
+impl MinInfo {
+    /// Parse minmal package info for every installed app
+    ///
+    /// # Errors
+    /// - Invalid file names
+    /// - File metadata errors
+    /// - Invalid time
+    pub fn list_installed(
+        ctx: &impl ScoopContext<config::Scoop>,
+        bucket: Option<&String>,
+    ) -> anyhow::Result<Vec<Self>> {
+        let apps = ctx.installed_apps()?;
+
+        apps.par_iter()
+        .map(Self::from_path)
+        .filter(|package| {
+            if let Ok(pkg) = package {
+                if let Some(bucket) = bucket {
+                    return &pkg.source == bucket;
+                }
+            }
+            // Keep errors so that the following line will return the error
+            true
+        })
+        .collect()
+    }
+
+    /// Parse minimal package into from a given path
+    ///
+    /// # Errors
+    /// - Invalid file names
+    /// - File metadata errors
+    /// - Invalid time
+    ///
+    /// # Panics
+    /// - Date time invalid or out of range
+    pub fn from_path(path: impl AsRef<Path>) -> anyhow::Result<Self> {
+        let path = path.as_ref();
+
+        let package_name = path
+            .file_name()
+            .map(|f| f.to_string_lossy()).context("Missing file name")?;
+
+        let updated_time = {
+            let updated = {
+                let updated_sys = path.metadata()?.modified()?;
+
+                updated_sys.duration_since(UNIX_EPOCH)?.as_secs()
+            };
+
+            #[allow(clippy::cast_possible_wrap)]
+            DateTime::from_timestamp(updated as i64, 0)
+                .expect("invalid or out-of-range datetime")
+                .with_timezone(&Local)
+        };
+
+        let app_current = path.join("current");
+
+        let (manifest_broken, manifest) =
+            if let Ok(manifest) = Manifest::from_path(app_current.join("manifest.json")) {
+                (false, manifest)
+            } else {
+                (true, Manifest::default())
+            };
+
+        let (install_manifest_broken, install_manifest) = if let Ok(install_manifest) =
+            InstallManifest::from_path(app_current.join("install.json"))
+        {
+            (false, install_manifest)
+        } else {
+            (true, InstallManifest::default())
+        };
+
+        let broken = manifest_broken || install_manifest_broken;
+
+        let mut notes = vec![];
+
+        if broken {
+            notes.push("Install failed".to_string());
+        }
+        if install_manifest.hold.contains_truth() {
+            notes.push("Held package".to_string());
+        }
+
+        Ok(Self {
+            name: package_name.to_string(),
+            version: manifest.version.to_string(),
+            source: install_manifest.get_source(),
+            updated: updated_time.into(),
+            notes: notes.join(", "),
+        })
+    }
+}
