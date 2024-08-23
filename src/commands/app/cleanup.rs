e5382198f63b78cb7ba1aa67d9dfbd7546b8571a
Juliette Cordor <professional@maybejules.com>
2024-08-23 20:48:06 +1000

    fixed command return

--- src/commands/app/cleanup.rs
+++ src/commands/app/cleanup.rs
@@ -4,7 +4,6 @@ use futures::future;
 use rayon::prelude::*;
 use sprinkles::{
     contexts::ScoopContext,
-    git::implementations::gix::date::time::format,
     handles::packages::PackageHandle,
     packages::{reference::package, CreateManifest, Manifest},
     progress::indicatif::{MultiProgress, ProgressBar},
@@ -97,6 +96,6 @@ impl super::Command for Args {
             apps_pb.inc(1);
         }
 
-        todo!()
+        Ok(())
     }
 }
