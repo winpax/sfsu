648c8d1d2b61e201378800bc92ec0fb891fa8249
Juliette Cordor <me@cordor.dev>
2026-02-14 13:38:47 +1100

    refactoring

--- src/git/pull.rs
+++ src/git/pull.rs
@@ -58,9 +58,12 @@ fn do_fetch<'a>(
     repo.reference_to_annotated_commit(&fetch_head)
 }
 
-fn gix_do_fetch<'a>(
-    remote: &'a mut gix::Remote<'_>,
+fn gix_do_fetch(
+    remote: &mut gix::Remote<'_>,
+    ref_specs: &[&str],
 ) -> Result<remote::fetch::Outcome, GitoxideError> {
+    remote.replace_refspecs(ref_specs, remote::Direction::Fetch)?;
+
     let outcome = remote
         .connect(remote::Direction::Fetch)?
         .prepare_fetch(progress::Discard, remote::ref_map::Options::default())?
@@ -190,11 +193,10 @@ pub fn pull(
     let mut remote = repo
         .gitoxide()
         .find_remote(remote_name)
-        .map_err(git::errors::GitoxideError::FindRemote)
-        .map_err(Box::new)
-        .map_err(git::Error::Gitoxide)?;
+        .map_err(git::errors::GitoxideError::from)
+        .map_err(git::Error::from)?;
 
-    gix_do_fetch(&mut remote)?;
+    gix_do_fetch(&mut remote, &[remote_branch])?;
 
     let oid = {
         let commit = repo.latest_remote_commit()?;
